ae4882c30866076e69789df521772bcea4c5c40e
tedspare <ted.spare@gmail.com>
2022-10-11 22:38:02 -0400

    Indicate distance from guess. Add comments.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,49 @@
 import Head from "next/head";
 import { useCallback, useEffect, useState } from "react";
 
+// Constants
+const GRID_SIZE = 6;
+const TIMEOUT_SHORT = 100; // milliseconds
+
+// Predefined objects
+const row = Array(GRID_SIZE).fill("");
+const grid = Array(GRID_SIZE).fill(row);
 const hexes = Array(16)
   .fill(0)
   .map((_, i) => i.toString(16));
 
-const row = Array(6).fill("");
-const grid = Array(6).fill(row);
+// Difference between two hexadecimal numbers
+const getDistance = (guess: any, answer: any) => {
+  const distance = parseInt(guess, 16) - parseInt(answer, 16);
+  const hotColdColor =
+    distance < -5
+      ? "bg-blue-600/50"
+      : distance < 0
+      ? "bg-blue-600/30"
+      : distance == 0
+      ? "bg-green-600/50"
+      : distance > 5
+      ? "bg-red-600/50"
+      : "bg-red-600/30";
+  return hotColdColor;
+};
 
-const keyClass =
-  "center h-16 w-12 rounded bg-gray-700 transition-transform hover:scale-110 hover:bg-gray-600";
+type GuessArray = Array<Array<string>>;
 
 interface IProps {
   hex: string;
 }
 
-type GuessArray = Array<Array<string>>;
-
+/**
+ * This is the main game component. It holds the title, grid, keyboard, and logic.
+ * @param {string} hex the hex code of the color to be guessed
+ */
 const Home = ({ hex }: IProps) => {
-  // Initialize guess array to 6x6 empty strings
+  // Initialize guess arrays to 6x6 empty strings
   const [guesses, setGuesses] = useState<GuessArray>(
+    [...Array(6)].map((_) => [...Array(6)].map((_) => ""))
+  );
+  const [distances, setDistances] = useState<GuessArray>(
     [...Array(6)].map((_) => [...Array(6)].map((_) => ""))
   );
 
@@ -36,27 +60,44 @@ const Home = ({ hex }: IProps) => {
     [currentRow, currentColumn]
   );
 
-  const handleEnter = (row: number, column: number, guesses: GuessArray) => {
-    if (column === 5 && !!guesses[row]?.[column]) {
-      if (guesses[row]?.join("") === hex) {
-        alert("You win!");
-      }
-      if (row === 5) {
-        alert("You lose!");
-      }
-      setCurrentRow((prev) => prev + 1);
-      setCurrentColumn(0);
+  // To evaluate the row when the user presses the enter key
+  const handleEnter = (row: number, guesses: GuessArray) => {
+    // Ensure all cells are filled
+    if (guesses[row]?.some((cell) => !cell)) return;
+
+    // Calculate distances to correct digit
+    guesses[row]?.forEach((guess, column: number) => {
+      setDistances((prev) => {
+        prev[row]![column] = getDistance(guess, hex[column]);
+        return prev;
+      });
+    });
+
+    // Win
+    if (guesses[row]?.join("") === hex) {
+      alert(`You win! The hex was #${hex}.`);
     }
+
+    // Lose
+    if (row === GRID_SIZE - 1) {
+      alert(`You lose! The hex was #${hex}.`);
+    }
+
+    // Move to next row and reset column
+    setCurrentRow((prev) => Math.min(prev + 1, GRID_SIZE - 1));
+    setCurrentColumn(0);
   };
 
+  // To clear the active cell when the user presses backspace
   const handleBackspace = (row: number, column: number) => {
-    setCurrentColumn((prev) => Math.max(prev - 1, 0));
     setGuesses((prev) => {
-      prev[row]![Math.max(column - 1, 0)] = "";
+      prev[row]![Math.max(column, 0)] = "";
       return prev;
     });
+    setCurrentColumn((prev) => Math.max(prev - 1, 0));
   };
 
+  // To populate the active cell when the user presses a key 0-F
   const handleKeystroke = (row: number, column: number, key: string) => {
     if (!hexes.includes(key)) return;
     setKeystroke(key);
@@ -64,31 +105,32 @@ const Home = ({ hex }: IProps) => {
       prev[row]![column] = key;
       return prev;
     });
-    setCurrentColumn((prev) => Math.min(prev + 1, 5));
+    setCurrentColumn((prev) => Math.min(prev + 1, GRID_SIZE - 1));
   };
 
+  // Listen for keyboard events
   useEffect(() => {
-    // Enter key: submit guess
-    // Backspace key: delete guess
-    // Arrow keys: move cursor
-    // Any other key: add to guess if valid hex character
     const keyboardHandler = ({ key }: KeyboardEvent) => {
       if (!key) return;
 
       switch (key) {
+        // Enter key: submit guess
         case "Enter":
-          handleEnter(currentRow, currentColumn, guesses);
+          handleEnter(currentRow, guesses);
           break;
+        // Backspace key: clear guess
         case "Backspace":
           handleBackspace(currentRow, currentColumn);
           break;
+        // Arrow keys: move cursor
         case "ArrowLeft":
           setCurrentColumn((prev) => Math.max(prev - 1, 0));
           break;
         case "ArrowRight":
           if (!guesses[currentRow]?.[currentColumn]) break;
-          setCurrentColumn((prev) => Math.min(prev + 1, 5));
+          setCurrentColumn((prev) => Math.min(prev + 1, GRID_SIZE - 1));
           break;
+        // Any other key: add to guess if valid hex character
         case hexes.includes(key) ? key : "":
           handleKeystroke(currentRow, currentColumn, key);
           break;
@@ -97,7 +139,6 @@ const Home = ({ hex }: IProps) => {
       }
     };
 
-    // Listen to keystrokes for 0-F characters
     window.addEventListener("keydown", keyboardHandler);
     return () => window.removeEventListener("keydown", keyboardHandler);
   }, [guesses, currentRow, currentColumn]);
@@ -107,7 +148,7 @@ const Home = ({ hex }: IProps) => {
     if (keystroke) {
       setTimeout(() => {
         setKeystroke("");
-      }, 100);
+      }, TIMEOUT_SHORT);
     }
   }, [keystroke]);
 
@@ -115,23 +156,29 @@ const Home = ({ hex }: IProps) => {
     <>
       <Head>
         <title>Hexle</title>
-        <meta name="description" content="Guess the hex code from a colour" />
+        <meta name="description" content="Guess the hex code from a color" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{ background: `#${hex}` }}>
-        <div className="center h-screen w-screen flex-col space-y-16 p-4">
-          <h1 className="text-5xl font-bold">Hexle</h1>
+      <main className="bg-black">
+        <div className="center h-screen w-screen flex-col space-y-20 p-4">
+          <div className="space-y-2 text-center">
+            <h1 style={{ color: `#${hex}` }}>Hexle</h1>
+            <h2>Guess the color of this title to win.</h2>
+            <p>Red means too high. Bright red means way too high. Etc.</p>
+          </div>
           {/* Guesses grid */}
           <div className="center flex-col space-y-2">
-            {grid.map((rowA: string[], i: number) => (
+            {grid.map((row: string[], i: number) => (
               <div key={i} className="center space-x-2">
-                {rowA.map((_, j: number) => (
+                {row.map((_, j: number) => (
                   <div
                     key={`${i}-${j}`}
                     className={`center h-12 w-12 rounded border ${
-                      isActive(i, j) ? "border-green-600" : "border-gray-600"
-                    }`}
+                      isActive(i, j)
+                        ? "scale-110 border-gray-400"
+                        : "border-gray-600"
+                    } ${distances[i]![j]} transition-transform`}
                   >
                     <span>{guesses?.[i]?.[j]}</span>
                   </div>
@@ -141,11 +188,11 @@ const Home = ({ hex }: IProps) => {
           </div>
           {/* Keyboard */}
           <div className="center max-w-xl flex-wrap gap-2">
-            {hexes.map((hex: string, i) => (
+            {hexes.map((hex: string, i: number) => (
               <div
                 key={i}
                 onClick={() => handleKeystroke(currentRow, currentColumn, hex)}
-                className={`${keyClass} ${
+                className={`keyboard ${
                   keystroke === hex ? "!bg-gray-500" : ""
                 }`}
               >
@@ -154,13 +201,13 @@ const Home = ({ hex }: IProps) => {
             ))}
             <div
               onClick={() => handleBackspace(currentRow, currentColumn)}
-              className={`${keyClass} px-2`}
+              className="keyboard px-2"
             >
               ←
             </div>
             <div
-              onClick={() => handleEnter(currentRow, currentColumn, guesses)}
-              className={`${keyClass} px-2`}
+              onClick={() => handleEnter(currentRow, guesses)}
+              className="keyboard px-2"
             >
               Enter
             </div>
@@ -175,7 +222,7 @@ export default Home;
 
 // To generate a random color
 export async function getServerSideProps() {
-  const hex = [...Array(6)]
+  const hex = [...Array(GRID_SIZE)]
     .map((_) => (~~(Math.random() * 16)).toString(16))
     .join("");
 
