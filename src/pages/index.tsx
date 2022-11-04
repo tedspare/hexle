32fb5bb3176329edc6a85c2e12e4cadfba320bb9
DexterStorey <dexter@dexterstorey.com>
2022-11-03 23:11:59 -0400

    formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -213,7 +213,7 @@ const Home = ({ hex }: IProps) => {
       <main
         style={{ backgroundColor: color }}
         className={
-          "flex h-full min-h-screen flex-col items-center overflow-y-scroll webkit-full-height"
+          "webkit-full-height flex h-full min-h-screen flex-col items-center overflow-y-scroll"
         }
       >
         {/* Header with title and info */}
